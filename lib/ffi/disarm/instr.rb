97f9ecdecddcb6a1a213c7eb993492913bbad633
Postmodern <postmodern.mod3@gmail.com>
2013-08-15 15:11:37 -0700

    Added Instr#conditional?

--- lib/ffi/disarm/instr.rb
+++ lib/ffi/disarm/instr.rb
@@ -6,8 +6,17 @@ module FFI
   module Disarm
     class Instr < FFI::Struct
 
+      NON_CONDITIONAL_GROUPS = [
+        :undef_1, :undef_2, :undef_3, :undef_4, :undef_5, :blk_imm
+      ]
+
       layout :data, :da_word_t,
              :group, :da_group_t
+
+      def conditional?
+        !NON_CONDITIONAL_GROUPS.include?(self[:group])
+      end
+
     end
   end
 end
